56e8bb062492316d92a51f1e78d8c35d7c0118e2
cindy <ciindy.wu@gmail.com>
2021-02-05 17:08:30 -1000

    Finish counter react-redux example

--- src/index.js
+++ src/index.js
@@ -2,39 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import {createStore} from 'redux';
+import allReducers from './reducers'
+import { Provider } from 'react-redux'
 
-//STORE -> GLOBALIZED STATE
+const store = createStore(
+  allReducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
-//ACTION INCREMENT
-const increment = () => {
-  return {
-    type: 'INCREMENT'
-  }
-}
-
-const decrement = () => {
-  return {
-    type: 'DECREMENT'
-  }
-}
-
-//REDUCER
-const counter = (state = 0, action) => {
-  switch(action.type) {
-    case "INCREMENT": 
-      return state + 1
-    case 'DECREMENT':
-      return state - 1
-  }
-}
-
-let store = createStore(counter)
-
-store.subscribe(() => console.log(store.getState()))
-
-//DISPATCH
-store.dispatch(increment())
-store.dispatch(decrement())
-store.dispatch(decrement())
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
